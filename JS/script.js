db853ef563e36de3a6ed05112ebe820196c26ede
DarleneLL <153782176+DarleneLL@users.noreply.github.com>
2024-03-12 17:36:04 -0300

    M1S08 exercicio 7

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// Remova uma classe de um elemento específico na página HTML.
+
+function removeClasse(){
+    let classeOriginal = document.querySelector('h1.classeOriginal')
+    classeOriginal.classList.remove('classeOriginal')
+}
